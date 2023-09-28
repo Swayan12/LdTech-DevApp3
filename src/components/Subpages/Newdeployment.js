7619bf6a7ad460a36de15e8263ee637f5089f283
sambit985 <kumarsambit678@gmail.com>
2023-09-28 16:22:44 +0530

    Updated Newdeployemnt :triangular_flag_on_post:

--- src/components/Subpages/Newdeployment.js
+++ src/components/Subpages/Newdeployment.js
@@ -1,44 +1,122 @@
-import React, { useState } from 'react';
-import '../Subpages/Newdeployment.css';
-import axios from 'axios'; // Import Axios
+import React, { useState } from "react";
+import axios from "axios";
 
+function Newdeployment() {
+  const [formData, setFormData] = useState({
+    pipelineName: '',
+    pipelineDescription: '',
+    gitRepoUrl: '',
+    gitBranch: '',
+    jenkinsfilePath: ''
+  });
 
-const Newdeployment = () => {
-  const [jobName, setJobName] = useState("");
-  const [responseMessage, setResponseMessage] = useState('');
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log('Request Payload:', formData);
+
+    // Check if all required fields are filled in
+    const requiredFields = ['pipelineName', 'gitRepoUrl', 'jenkinsfilePath'];
+    const missingFields = requiredFields.filter(field => !formData[field]);
+    if (missingFields.length > 0) {
+      alert(`Please fill in the following required fields: ${missingFields.join(', ')}`);
+      return;
+    }
+
+    // Create the Axios request body
+    const requestBody = {
+      pipelineName: formData.pipelineName,
+      pipelineDescription: formData.pipelineDescription,
+      gitRepoUrl: formData.gitRepoUrl,
+      gitBranch: formData.gitBranch,
+      jenkinsfilePath: formData.jenkinsfilePath
+    };
+    
+
+    // Send the Axios request
     try {
-      const response = await axios.post("http://13.234.23.179:3004/api/create-job", {
-        name: jobName,
-      });      
+      const response = await axios.post('http://13.234.23.179:9090/api/jenkins/createPipeline', requestBody);
 
       if (response.status === 200) {
-        // Display success message to the user.
-        setResponseMessage("jenkin job created successfully");
+        alert('Jenkins pipeline job created successfully');
       } else {
-        // Display error message to the user.
-        setResponseMessage('Error creating Jenkins job.');
+        alert('Error creating Jenkins pipeline job');
+
+        // Log the error to the console for debugging
+        console.log(response);
       }
     } catch (error) {
-      // Display error message to the user.
-      console.log("Error while creating job by user:", error);
-      setResponseMessage('An error occurred while creating the Jenkins job.');
+      alert("Error occured");
+      console.error('Error:', error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Job name"
-        value={jobName}
-        onChange={(e) => setJobName(e.target.value)}
-      />
-      <button type="submit">Create job</button>
-      {responseMessage && <p>{responseMessage}</p>}
-    </form>
+    <div>
+      <h2>Create Jenkins Pipeline Job</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="pipelineName">Pipeline Name:</label>
+          <input
+            type="text"
+            id="pipelineName"
+            name="pipelineName"
+            value={formData.pipelineName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="pipelineDescription">Pipeline Description:</label>
+          <input
+            type="text"
+            id="pipelineDescription"
+            name="pipelineDescription"
+            value={formData.pipelineDescription}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="gitRepoUrl">Git Repo URL:</label>
+          <input
+            type="text"
+            id="gitRepoUrl"
+            name="gitRepoUrl"
+            value={formData.gitRepoUrl}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="gitBranch">Git Branch:</label>
+          <input
+            type="text"
+            id="gitBranch"
+            name="gitBranch"
+            value={formData.gitBranch}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="jenkinsfilePath">Jenkinsfile Path:</label>
+          <input
+            type="text"
+            id="jenkinsfilePath"
+            name="jenkinsfilePath"
+            value={formData.jenkinsfilePath}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Create Job</button>
+        </div>
+      </form>
+    </div>
   );
-};
+}
 
 export default Newdeployment;
